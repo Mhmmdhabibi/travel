34396a7aa093be9a7986177e1e03e483bbfb40ff
Rizkommam111 <rizkiko368@gmail.com>
2024-06-21 11:27:37 +0700

    ADD

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('no_telp')->unique()->nullable();
-            $table->enum('role', ['admin', 'user']);
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
